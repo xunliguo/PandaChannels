6ec64c3d65bee360683142c0ccf205d7e168c6d4
xunliguo <1282775652@QQ.com>
2017-09-16 19:25:56 +0800

    qi

--- app/src/main/java/com/example/administrator/pandachannels/fragmentlive/LiveVideoActivity.java
+++ app/src/main/java/com/example/administrator/pandachannels/fragmentlive/LiveVideoActivity.java
@@ -2,19 +2,18 @@ package com.example.administrator.pandachannels.fragmentlive;
 
 
 import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.VideoView;
 
 
 import com.example.administrator.pandachannels.R;
 
-import io.vov.vitamio.MediaPlayer;
-import io.vov.vitamio.Vitamio;
-import io.vov.vitamio.widget.MediaController;
-import io.vov.vitamio.widget.VideoView;
 
-public class LiveVideoActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener {
+
+public class LiveVideoActivity extends AppCompatActivity  {
 
     private VideoView vitamio_viedoview;
 
@@ -26,23 +25,15 @@ public class LiveVideoActivity extends AppCompatActivity implements MediaPlayer.
     }
 
     private void initView() {
-        vitamio_viedoview = (VideoView) findViewById(R.id.vitamio_viedoview);
+
         Intent intent = getIntent();
         String url1 = intent.getStringExtra("url");
         Log.e("TAG","=============>"+url1);
 
-        Vitamio.isInitialized(this);
-        String url="https://vfx.mtime.cn/Video/2017/07/27/mp4/170727100951771139.mp4";
-        vitamio_viedoview.setVideoPath(url1);
-        vitamio_viedoview.setOnPreparedListener(this);
 
-        vitamio_viedoview.setMediaController(new MediaController(this));
     }
 
-    @Override
-    public void onPrepared(MediaPlayer mp) {
-        vitamio_viedoview.start();
-    }
+
 
 
 
